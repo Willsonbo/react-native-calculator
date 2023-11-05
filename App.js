e508ed4d825275ed6d65087a0f23280d144141d6
Leo Wang <ab0988956087@gmail.com>
2023-11-05 15:20:19 +0800

    feat(pad-button): create buttons to click

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import PadButton from './components/PadButton';
 
 export default function App() {
+  const [expression, setExpression] = useState('');
+
+  const labels = [
+    '7', '8', '9', '*',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    'C', '0', '=', '/',
+  ];
+
+  const onPressButton = (label) => {
+    switch (label) {
+      case '':
+        return;
+      case 'C':
+        setExpression('');
+        break;
+      default:
+        // TODO: implement
+        console.log(`${label} is clicked`);
+        setExpression(`${expression}${label}`)
+        break;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.expression} >{expression}</Text>
+      <FlatList
+        style={styles.buttonList}
+        numColumns={4}
+        data={labels}
+        keyExtractor={(item) => item} // can label as key
+        renderItem={({ item: label }) => (
+          <PadButton label={label} onPress={onPressButton} color={label === 'C' ? '#E06C75' : 'white'} />
+        )} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#252931',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  buttonList: {
+    flex: 1
+  },
+  expression: {
+    textAlignVertical: 'bottom',
+    alignItems: 'flex-end',
+    width: '100%',
+    color: 'white',
+    fontSize: 40,
+    marginRight: 40,
+    textAlign: 'right',
+    flex: 1
+  }
 });
---
+++ components/PadButton.js
@@ -0,0 +1,26 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native"
+
+const PadButton = ({ label, color = 'white', onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button}
+      onPress={() => onPress(label)} >
+      <Text style={[styles.buttonText, { color: color }]}>{label}</Text>
+    </TouchableOpacity >
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 24
+  },
+  button: {
+    width: 80,
+    height: 80,
+    margin: 2.5,
+    justifyContent: 'center',
+    backgroundColor: '#98C379'
+  }
+})
+
+export default PadButton
